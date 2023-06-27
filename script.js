f718bf1cae29e8f18dcda0aa53fa438872aab14d
Amit-Yadav21 <amit21@navgurukul.org>
2023-06-27 12:57:42 +0530

    change

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("start").addEventListener("click", function () {
     makeSound();
     let p1 = Math.floor(Math.random() * 6) + 1;
     let p2 = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("p1").src = `./image/${p1}.png`;
+    document.getElementById("p1").src = `./image${p1}.png`;
     document.getElementById("p2").src = `./image/${p2}.png`;
     if (p1 > p2) {
         document.querySelector("h1").textContent = "🎋Player 1 wins🎋";
